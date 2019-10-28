f1a7cabbc0045973bd4e0e0d70250c0e254e3466
s3rfin <serfin009@gmail.com>
2019-10-28 23:29:42 +0100

    Added build-in logging for bus subscriber

--- src/BeComfy.Common/RabbitMq/BusSubscriber.cs
+++ src/BeComfy.Common/RabbitMq/BusSubscriber.cs
@@ -5,17 +5,20 @@ using BeComfy.Common.CqrsFlow;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using RawRabbit;
+using Microsoft.Extensions.Logging;
 
 namespace BeComfy.Common.RabbitMq
 {
     public class BusSubscriber : IBusSubscriber
     {
+        private readonly ILogger _logger;
         private readonly IBusClient _busClient;
         private readonly IServiceProvider _serviceProvider;
         private readonly int _retries;
         private readonly int _retryInterval;
         public BusSubscriber(IApplicationBuilder app)
         {
+            _logger = app.ApplicationServices.GetService<ILogger<BusSubscriber>>();
             _serviceProvider = app.ApplicationServices.GetService<IServiceProvider>();
             _busClient = _serviceProvider.GetService<IBusClient>();
             var options = _serviceProvider.GetService<RabbitMqOptions>();
@@ -28,9 +31,19 @@ namespace BeComfy.Common.RabbitMq
         {
             await _busClient.SubscribeAsync<TCommand, CorrelationContext>(async (command, correlationContext) =>
             {
+                var now = DateTime.Now;
+                _logger.LogInformation(
+                    $"[{now}] Received message '{command.GetType().Name}'" 
+                        + $" with correlationId '{correlationContext.Id}'");
+
                 var commandHandler = _serviceProvider.GetService<ICommandHandler<TCommand>>();
 
                 await commandHandler.HandleAsync(command, correlationContext);
+
+                _logger.LogInformation(
+                    $"[{DateTime.Now}] Handled message '{command.GetType().Name}'" 
+                        + $" with correlationId '{correlationContext.Id}'"
+                        + $" [{DateTime.Now.Subtract(now).Milliseconds} ms]");
             });
         }
     }
