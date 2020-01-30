19c604ed080f5c8fb7b8684f761e6b7d14df990f
serfin <serfin009@gmail.com>
2020-01-30 20:50:50 +0100

    Added code to exceptions

--- src/BeComfy.Common/Types/Exceptions/BeComfyException.cs
+++ src/BeComfy.Common/Types/Exceptions/BeComfyException.cs
@@ -4,19 +4,36 @@ namespace BeComfy.Common.Types.Exceptions
 {
     public class BeComfyException : Exception
     {
+        public string Code { get; }
+
         public BeComfyException()
         {
-
         }
 
-        public BeComfyException(string message) : base(message)
+        public BeComfyException(string code)
         {
-
+            Code = code;
         }
 
-        public BeComfyException(string message, Exception innerException) : base(message, innerException)
+        public BeComfyException(string message, params object[] args) 
+            : this(string.Empty, message, args)
         {
-
         }
+
+        public BeComfyException(string code, string message, params object[] args) 
+            : this(null, code, message, args)
+        {
+        }
+
+        public BeComfyException(Exception innerException, string message, params object[] args)
+            : this(innerException, string.Empty, message, args)
+        {
+        }
+
+        public BeComfyException(Exception innerException, string code, string message, params object[] args)
+            : base(string.Format(message, args), innerException)
+        {
+            Code = code;
+        } 
     }
 }
