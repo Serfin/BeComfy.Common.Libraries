80813b57fe894aa70a32aedd7771f93411ef7b41
serfin <serfin009@gmail.com>
2020-02-03 23:29:00 +0100

    Added RestEase config provider

--- src/BeComfy.Common/RestEase/Extensions.cs
+++ src/BeComfy.Common/RestEase/Extensions.cs
@@ -1,3 +1,6 @@
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using RestEase;
 
@@ -5,13 +8,26 @@ namespace BeComfy.Common.RestEase
 {
     public static class Extensions
     {
-        public static void RegisterRestClientFor<T>(this IServiceCollection services, string url)
+        private static readonly string reastEaseSection = "restEase";
+        public static void RegisterRestClientFor<T>(this IServiceCollection services, string serviceName)
             where T : class
         {
-            services.AddTransient<T>(c => new RestClient(url)
+            IConfiguration config;
+
+            using (var serviceProvider = services.BuildServiceProvider())
+            {
+                config = serviceProvider.GetService<IConfiguration>();
+            }
+
+            IEnumerable<RestEaseService> restEaseServices = config.GetOptions<List<RestEaseService>>(reastEaseSection);
+
+            services.AddTransient<T>(c => new RestClient(BuildServiceUrl(restEaseServices.SingleOrDefault(x => x.Name == serviceName)))
             {
                 RequestQueryParamSerializer = new QueryParamSerializer()
             }.For<T>());
         }
+
+        private static string BuildServiceUrl(RestEaseService service)
+            => $"{service.Protocol}://{service.Host}:{service.Port}";
     }
 }
---
+++ src/BeComfy.Common/RestEase/RestEaseService.cs
@@ -0,0 +1,10 @@
+namespace BeComfy.Common.RestEase
+{
+    public class RestEaseService
+    {
+        public string Name { get; set; }
+        public string Protocol { get; set; }
+        public string Host { get; set; }
+        public int Port { get; set; }
+    }
+}
